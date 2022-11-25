b44d30ad2b36c92d686cd2e4648e0f1f95b26a37
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-11-24 22:01:59 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo cat > /bin/menu-update <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC EasyFreeDMR Menu Update" --menu "move up or down with the keyboard arrows and select your option by pressing enter:" 23 56 13 \
+1 " Update FreeDMR Server " \
+2 " Main menu " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+update-fdmr;;
+2)
+breack;
+esac
+done
+exit 0
+
+
+
+
+EOF
+########################
+chmod +x /bin/menu-update
+menu-update
