455fff8277ef0f46f4ed596ccdd16b077ac04051
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-12-29 00:15:22 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/install.sh)"
 start-fdmr
+sudo cat > /opt/extra.sh <<- "EOF"
+#!/bin/bash
+######################################################################
+# Coloque en este archivo, cualquier instruccion shell adicional que # 
+# quierre se realice al finalizar un Full Upgrade desde el menu.     #
+######################################################################
+ 
+EOF
+sudo cat > /opt/obp.txt <<- "EOF"
+######################################################################
+# Coloque abajo su lista de obp y peer para que se agreguen          #
+al finalizar un Full Upgrade desde el menu.                          #
+######################################################################
+ 
+
+EOF
+
+
 history -c && history -w
 menu
