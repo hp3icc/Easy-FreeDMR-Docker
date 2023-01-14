35d05dc1016293787c838f8e7a5b35a4a66a5409
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-01-14 06:54:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -459,43 +459,7 @@ chmod -R 777 /etc/freedmr/hbmon/log
 chmod 755 /etc/freedmr -R
 
 #############################
-cat > /bin/menu <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC EasyFreeDMR Docker Version" --menu "move up or down with the keyboard arrows and select your option by pressing enter:" 17 56 8 \
-1 " Edit FreeDMR Server " \
-2 " Edit Interlink  " \
-3 " Edit FDMR-Monitor  " \
-4 " Start & Restart FreeDMR Server  " \
-5 " Stop FreeDMR Server " \
-6 " update " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-nano /etc/freedmr/freedmr.cfg ;;
-2)
-nano /etc/freedmr/rules.py ;;
-3)
-nano /etc/freedmr/hbmon/fdmr-mon.cfg  ;;
-4)
-start-fdmr ;;
-5)
-stop-fdmr ;;
-6)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/update.sh)";
-esac
-done
-exit 0
-EOF
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/menu.sh)";
 ##
 cp /bin/menu /bin/MENU
 #
