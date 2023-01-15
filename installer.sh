99e212535081b835e8af192e74fd856406648022
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-01-15 12:08:06 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/custom.sh)"
+menu
