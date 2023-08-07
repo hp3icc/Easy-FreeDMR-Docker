99865b452f7ade46fbb5cabeccf06a0954048fd7
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-08-07 08:54:45 -0500

    Delete installer.sh

--- installer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/install.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/Easy-FreeDMR-Docker/main/custom.sh)"
-menu
